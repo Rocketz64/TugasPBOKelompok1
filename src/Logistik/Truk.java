8ab7c72f9442df21f5bf213203a7b5825357e8a5
muhammad alfaiq rido salafy <alfaiqrido@gmail.com>
2026-03-31 19:50:32 +0700

    Final

--- src/Logistik/Truk.java
+++ src/Logistik/Truk.java
@@ -1,3 +1,12 @@
+/*
+Nama File: Truk.java
+Anggota:
+- Hadrian Shandhy Yudha 		(24060124140207)
+- Muhammad Alfaiq Rido Salafy	(24060124140148)
+- Mohammad Najib Fitrianto      (24060124140203)
+- Ridho Tri Saputra  	        (24060124130122)
+*/
+
 package Logistik;
 
 public class Truk extends Kendaraan implements PelacakanGPS{
